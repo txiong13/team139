01f99778e6a48fa0c98c87f4c95f0a7babeb38c9
Thomas Xiong <txiong@mit.edu>
2020-03-28 18:01:33 -0500

    Spec and implement family pool strategy

--- src/test_strategies.py
+++ src/test_strategies.py
@@ -1,11 +1,13 @@
 import networkx as nx
 from random import sample
 
+FAMILY_CLIQUE_SIZE = 4
+
 def test_strat_random_sample(graph, n):
     '''
     Randomly select n nodes of the graph to be tested; tests those nodes and mutates graph accordingly.
 
-    Paramters: 
+    Parameters: 
         "graph" : NetworkX graph to be analyzed
         "n" : Number of nodes to be tested; nonnegative integer and must be no larger than the number of nodes in the graph.
     Returns:
@@ -20,7 +22,7 @@ def test_strat_high_contact(graph, d):
     '''
     Test all nodes with degree greater than or equal to d in graph and mutates graph accordingly.
 
-    Paramters: 
+    Parameters: 
         "graph" : NetworkX graph to be analyzed
         "d" : nonnegative integer; nodes with degree greater than or equal to d will be tested.
     Returns:
@@ -36,6 +38,21 @@ def test_strat_high_contact(graph, d):
     #update_postiive_test(graph, tested_nodes)
     return tested_nodes
 
+def test_strat_pool_family(graph):
+    '''
+    Test all nodes that are part of families (defined as maximal cliques of specified size or greater) in graph. Mutates graph accordingly.
+
+    Parameters:
+        "graph" : NetworkX graph to be analyzed
+    Returns:
+        list of nodes that were tested
+    '''
+
+    tested_nodes = []
+    max_cliques = nx.find_cliques(graph)
+    tested_nodes = [clique for clique in max_cliques if len(clique) >= FAMILY_CLIQUE_SIZE]
+    return tested_nodes
+
 
 g = nx.Graph()
 g.add_edge(1,2)
@@ -46,4 +63,5 @@ g.add_edge(5,6)
 
 print(test_strat_random_sample(g, 1))
 print(test_strat_high_contact(g, 4))
+print(test_strat_pool_family(g))
 
